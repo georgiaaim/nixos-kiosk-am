aa367f099f02d22475aadbf3e332b0ab3994d47d
Hunter <hdh20267@uga.edu>
2024-03-18 13:16:39 -0400

    am first commit

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    kioskBase.url = "github:georgiaaim/nixos-kiosk-base"; # Adjust the URL/path to your flake
+    kioskBase.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, kioskBase, ... }: {
+    nixosConfigurations.AMVignette = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        kioskBase.nixosModules.baseEnvironment
+        ./hardware-configuration.nix # The consumer's specific hardware configuration
+        ({ pkgs, ... }: {
+          # Any additional system-specific configuration
+          networking.hostName = "AMVignette";
+        })
+      ];
+    };
+  };
+}
+
